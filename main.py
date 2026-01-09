e90e802487f4d28810c1904e7b0b7f38a4a4a576
seran261 <seran261@gmail.com>
2026-01-09 23:03:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from scanner import fetch_top_symbols, scan_symbol
+from telegram import send_signal
+from config import TIMEFRAMES, TOP_COINS_LIMIT
+import time
+
+symbols = fetch_top_symbols(TOP_COINS_LIMIT)
+
+while True:
+    for tf in TIMEFRAMES:
+        for symbol in symbols:
+            try:
+                signal = scan_symbol(symbol, tf)
+                if signal:
+                    send_signal(signal)
+                    time.sleep(2)
+            except Exception as e:
+                print(symbol, e)
+
+    time.sleep(3600)  # scan every hour
